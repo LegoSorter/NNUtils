5cf617b2251ddf50f47f898d7bd35a95f7d82a94
kzawora <konrad.zawora@outlook.com>
2021-05-24 23:14:08 +0000

    Overhaul train script

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from tensorflow.python.keras.callbacks import EarlyStopping
 import wandb
 import numpy as np
 import random
@@ -9,21 +10,13 @@ from tensorflow.keras.applications import ResNet50, EfficientNetB0, EfficientNet
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import os
+import logging
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 
 gpus = tf.config.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-
-
-class Config():
-    IMG_SIZE = 224
-    NUM_CLASSES = 5
-    MODEL = 'EfficientNetB0'
-    TRANSFER_LEARNING = True
-    ONLINE_AUGMENTATION = True
-    BATCH_SIZE = 128
 
 
 img_augmentation = Sequential(
@@ -47,11 +40,11 @@ class ModelBuilder():
     def __init__(self, cfg):
         self.cfg = cfg
 
-    def __generic_builder(self, name, net, lr=1e-2, dropout_rate=0.2):
+    def generic_builder(self, name, net, lr=1e-2, dropout_rate=0.2):
         cfg = self.cfg
-        inputs = layers.Input(shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
-        x = img_augmentation(inputs) if cfg.ONLINE_AUGMENTATION else inputs
-        if cfg.TRANSFER_LEARNING:
+        inputs = layers.Input(shape=cfg['img_shape'])
+        x = img_augmentation(inputs)
+        if cfg['transfer_learning']:
             model = net(include_top=False, input_tensor=x, weights='imagenet')
 
             # Freeze the pretrained weights
@@ -60,17 +53,17 @@ class ModelBuilder():
             # Rebuild top
             x = layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
             x = layers.BatchNormalization()(x)
-            top_dropout_rate = 0.2
+            top_dropout_rate = dropout_rate
             x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
             outputs = layers.Dense(
-                cfg.NUM_CLASSES, activation="softmax", name="pred")(x)
+                cfg['num_classes'], activation="softmax", name="pred")(x)
         else:
             model = net(include_top=False, input_tensor=x, weights=None)
             model.trainable = True
-            top_dropout_rate = 0.2
+            top_dropout_rate = dropout_rate
             x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
             outputs = layers.Dense(
-                cfg.NUM_CLASSES, activation="softmax", name="pred")(x)
+                cfg['num_classes'], activation="softmax", name="pred")(x)
 
         # Compile
         model = tf.keras.Model(inputs, outputs, name=name)
@@ -80,88 +73,113 @@ class ModelBuilder():
         )
         return model
 
-    def build_efficientnetb0(self):
-        return self.__generic_builder('EfficientNetB0', EfficientNetB0)
-
-    def build_resnet50v2(self):
-        return self.__generic_builder('ResNet50V2', ResNet50V2)
-
-    def build_resnet50(self):
-        return self.__generic_builder('ResNet50', ResNet50)
-
-    def build_inceptionv3(self):
-        return self.__generic_builder('InceptionV3', InceptionV3)
-
 
 class ModelSelector():
 
     def get_model_map(self):
         return {
-            'EfficientNetB0': self.mb.build_efficientnetb0,
-            'ResNet50': self.mb.build_resnet50,
-            'ResNet50V2': self.mb.build_resnet50v2,
-            'InceptionV3': self.mb.build_inceptionv3
+            'EfficientNetB0': EfficientNetB0,
+            'ResNet50': ResNet50,
+            'ResNet50V2': ResNet50V2,
+            'InceptionV3': InceptionV3
         }
 
     def __init__(self, cfg):
         self.cfg = cfg
-        model_name = self.cfg.MODEL
+        model_name = self.cfg['model']
         self.mb = ModelBuilder(cfg)
         try:
-            self.model = self.get_model_map()[model_name]()
+            model_module = self.get_model_map()[model_name]
+            self.model = self.mb.generic_builder(model_name, model_module, lr=self.cfg['pre_training_learning_rate'])
         except KeyError:
             raise Exception(f'Invalid model name: {model_name}')
 
     def get_model(self):
         return self.model
 
-    def unfreeze_model(self):
+    def unfreeze_top_n_layers(self, n: int, lr=None):
+        lr = self.model.optimizer.lr.numpy() if lr is None else lr
         model = self.model
-        for layer in model.layers[-20:]:
+        for layer in model.layers[-n:]:
             if not isinstance(layer, layers.BatchNormalization):
                 layer.trainable = True
 
-        optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
         model.compile(
             optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy", "top_k_categorical_accuracy"]
         )
         return model
 
-
 if __name__ == '__main__':
-    wandb.init(project='lego-test',
+    wandb.init(project='lego447classes',
                config={
-                   'learning_rate': 1e-2,
-                   'epochs': 10,
+                   'model': 'EfficientNetB0',
+                   'img_shape': (224, 224, 3),
+                   'max_epochs': 100,
+                   'num_classes': 447,
                    'batch_size': 128,
-                   'loss_function': "categorical_crossentropy",
                    'architecture': 'CNN',
-                   'dataset': 'LEGO'
+                   'dataset': 'LEGO_447c',
+                   'wandb_val_images': 10,
+                   'transfer_learning': True,
+                   'pre_training_only': False,
+                   'pre_training_learning_rate': 1e-2,
+                   'pre_training_min_delta': 0.01,
+                   'pre_training_patience': 3,
+                   'fine_tuning_learning_rate': 1e-4,
+                   'fine_tuning_min_delta': 0,
+                   'fine_tuning_patience': 0,
+                   'fine_tuning_unfreeze_interval': 10
                })
-    wandb_cfg = wandb.config
-
-    cfg = Config()
+    cfg = wandb.config
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
     ms = ModelSelector(cfg)
     model = ms.get_model()
-    train_datagen = ImageDataGenerator()#rescale=1. / 255)
+    width, height, depth = cfg['img_shape']
+    train_datagen = ImageDataGenerator()  # rescale=1. / 255)
     train_generator = train_datagen.flow_from_directory(
-        '/macierz/home/s165115/legosymlink/kzawora/small_test/train',
-        target_size=(cfg.IMG_SIZE, cfg.IMG_SIZE),
-        batch_size=cfg.BATCH_SIZE)
-
-    val_datagen = ImageDataGenerator()#rescale=1. / 255)
-    val_generator = train_datagen.flow_from_directory(
-        '/macierz/home/s165115/legosymlink/kzawora/small_test/val',
-        target_size=(cfg.IMG_SIZE, cfg.IMG_SIZE),
-        batch_size=cfg.BATCH_SIZE)
+        '/macierz/home/s165115/legosymlink/kzawora/dataset_processed/train',
+        target_size=(width, height),
+        batch_size=cfg['batch_size'], shuffle=True)
+    val_datagen = ImageDataGenerator()  # rescale=1. / 255)
+    val_generator = val_datagen.flow_from_directory(
+        '/macierz/home/s165115/legosymlink/kzawora/dataset_processed/val',
+        target_size=(width, height),
+        batch_size=cfg['batch_size'], shuffle=True)
 
     wandb_callback = WandbCallback(data_type='image',
-              training_data=val_generator[1],
-              labels=list(train_generator.class_indices.keys()))
+                                   training_data=val_generator[0][:cfg['wandb_val_images']],
+                                   labels=list(train_generator.class_indices.keys()))
 
-    model.fit(train_generator, validation_data=val_generator, epochs=15, callbacks=[wandb_callback])
-    
-    if cfg.TRANSFER_LEARNING:
-        model = ms.unfreeze_model()
-        model.fit(train_generator, validation_data=val_generator, epochs=15, callbacks=[wandb_callback])
+    pre_training_early_stopping_callback = EarlyStopping(
+        monitor='val_accuracy', mode='max', min_delta=cfg['pre_training_min_delta'], patience=cfg['pre_training_patience'], restore_best_weights=True)
+    pre_training_callbacks = [wandb_callback,
+                              pre_training_early_stopping_callback]
 
+    fine_tuning_early_stopping_callback = EarlyStopping(
+        monitor='val_loss', mode='min', min_delta=cfg['fine_tuning_min_delta'], patience=cfg['fine_tuning_patience'], restore_best_weights=True)
+    fine_tuning_callbacks = [wandb_callback,
+                             fine_tuning_early_stopping_callback]
+
+    max_epochs_per_fit = 50
+    epochs_to_date = 0
+    # pre-training
+    wandb.log({'trainable_layers': len(
+        [1 for layer in model.layers if layer.trainable is True])})
+    history = model.fit(train_generator, validation_data=val_generator,
+                        epochs=max_epochs_per_fit, callbacks=pre_training_callbacks)
+    if cfg['transfer_learning'] and not cfg['pre_training_only']:
+        for to_unfreeze in range(cfg['fine_tuning_unfreeze_interval'], len(model.layers), cfg['fine_tuning_unfreeze_interval']):
+            model = ms.unfreeze_top_n_layers(
+                to_unfreeze, lr=cfg['fine_tuning_learning_rate'])
+            trainable_layers = len([1 for layer in model.layers if layer.trainable is True])
+            wandb.log({'trainable_layers': trainable_layers})
+            logging.info(f'Fine-tuning on {trainable_layers}')
+            epochs_to_date += len(history.history['loss'])
+            if epochs_to_date >= cfg['max_epochs']:
+                break
+            epochs_to_do = epochs_to_date+max_epochs_per_fit if epochs_to_date + \
+                max_epochs_per_fit < cfg['max_epochs'] else cfg['max_epochs']
+            history = model.fit(train_generator, validation_data=val_generator, initial_epoch=epochs_to_date,
+                                epochs=epochs_to_do, callbacks=fine_tuning_callbacks)
